f5c4c9ad8cf4ace415cd89a5e65ee6648bd2ed58
trungquan <trungquana69g@gmail.com>
2013-06-08 16:58:21 +0700

    Task #3654 Sign up view

---
+++ protected/views/user/forget_password.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<div class="none"></div>
+<div style="font-size: 2em; color:red; text-align: center">Password Recovery</div>
+
+<div class="row">
+    <form method="Post">
+       <fieldset class="six centered columns">
+         <div style="height:50px"></div>
+          <div class="field">
+            <input class="text input" id="email" name='ForgetPasswordForm["arg"]' type="text" placeholder="Enter your Username or Email or Employee code" />
+          </div>
+          <div style="height:50px"></div>
+          <div class="medium primary btn centered three columns"><input type="submit" value="Submit"></div>
+       </fieldset>
+    </form>
+</div>
+<div style="height:150px"></div>
---
+++ protected/views/user/signup.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<div style="height:50px"></div>
+<div class="row">
+<div class="centered seven columns">
+    <div style="font-size: 3em; color:red; text-align:center">Welcome to our website</div></div></div>
+
+<div class="none"></div>
+
+<div class="row">
+    <div class="centered eight columns" style="color: blue; font-size: 1.3em">
+      <form method='post'>  
+          <div class="row"> <div class="field"><label class="inline label-name-normal">Username</label><input class="wide text input" id="username_input" name='SignUpForm["username"]' type="text" placeholder="Username" /></div></div>
+          <div class="row"> <div class="field"><label class="inline label-name-normal">Password</label><input class="wide password input" id="password_input" name='SignUpForm["passord"]' type="password" placeholder="Password" /></div></div>
+          <div class="row"> <div class="field"><label class="inline label-name-normal">Password Confirm</label><input class="wide password input" id="password-confirm_input" name='SignUpForm["passwordConfirm"]' type="password" placeholder="Password" /></div></div>
+
+          <div style="height:50px"></div>
+          
+      <div class="medium primary btn centered two columns"><a href="#">Sign Up</a></div>
+    </form>
+    </div>
+</div>
+
+<div style="height:120px"></div>
