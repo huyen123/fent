391122c48a6612b6f0d571a5a301bd721f69ad73
Van Giang <giangnv91@gmail.com>
2013-06-28 17:54:32 +0700

    Task #4644 Add a filter to view only non-busy devices

--- protected/controllers/CategoryController.php
+++ protected/controllers/CategoryController.php
@@ -2,19 +2,28 @@
 
 class CategoryController extends Controller
 {
-    public function actionView($id)
+    public function actionView($id, $display = null)
     {
         $category = Category::model()->findByPk($id);
         $criteria = new CDbCriteria();
-        $criteria->condition = 'category_id=:category_id';
-        $criteria->params = array(':category_id' => $id);
+        $params = array();
+        if (isset($_GET['display'])) {
+            $display = $_GET['display'];
+            $criteria->addCondition('status=:status');
+            $criteria->addCondition('id not in (select device_id from request where status = :accepted)');
+            $params[':status'] = Constant::$DEVICE_NORMAL;
+            $params[':accepted'] = Constant::$REQUEST_ACCEPTED;
+        }
+        $criteria->addCondition('category_id=:category_id');
+        $params[':category_id'] = $id;
+        $criteria->params = $params;
         $count = Device::model()->count($criteria);
         $pages = new CPagination($count);
         $pages->pageSize = 6;
         $pages->applyLimit($criteria);
         $devices = Device::model()->findAll($criteria);
         $this->render('index', array('category' => $category, 'devices' => $devices,
-            'id' => $id, 'pages' => $pages, 'columns' => 2));
+            'id' => $id, 'pages' => $pages, 'columns' => 2, 'display' => $display));
     }
 }
 ?>
